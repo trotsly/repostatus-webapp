aba85e1e7a6d37b3850a4661471c43527fbea74b
Deepjyoti Barman <deep.barman30@gmail.com>
2020-10-26 12:23:53 +0530

    Some more changes to the repo choose button
    Made some visual changes to make the repo choose button look better.

--- src/components/InputRepo.vue
+++ src/components/InputRepo.vue
@@ -1,14 +1,26 @@
 <template>
   <div id="input-repo">
     <div class="d-flex justify-content-center py-5 mt-3">
-      <div class="py-5 repo-chooser"></div>
+      <div class="repo-chooser">
+        <span>
+          <GithubIcon class="github-icon" size="30" />
+        </span>
+        <span class="repo-text">
+          <p>Choose GitHub Repo</p>
+        </span>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import { GithubIcon } from "vue-feather-icons";
+
 export default {
-  name: "InputRepo"
+  name: "InputRepo",
+  components: {
+    GithubIcon
+  }
 };
 </script>
 
@@ -21,5 +33,21 @@ export default {
   background: $green;
   cursor: pointer;
   color: $black;
+  padding: 25px 10px;
+  display: flex;
+  justify-content: center;
+
+  .github-icon {
+    margin-right: 5px;
+  }
+
+  .repo-text {
+    margin-left: 8px;
+    font-size: 1.1em;
+
+    p {
+      margin: 0 auto;
+    }
+  }
 }
 </style>
